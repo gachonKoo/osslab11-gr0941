ab356d4f097c3f2d3241ea00fdef66dc78351e98
son77 <son77915124@gmail.com>
2025-11-14 16:58:17 +0900

    fix2 typo

--- lab11/geo/utils.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def distance(p1, p2):
-	x1, y1 = p1
-	x2, y2 = p2
-	return math.sqrt((x2-x1)**2 + (y2 - y1)**2)
--- tester.py
+++ tester.py
@@ -3,7 +3,7 @@ import geo.utils as utils
 #calculate the length of hypotenuse(c) when a=3 and b=4
 
 a, b = 3, 4
-c = utils.pythagross(a, b)
+c = utils.pythagrass(a, b)
 print('c =', c)
 
 
